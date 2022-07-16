69a1b9295550866eae4b2b9dd487fe07d4dc9b40
jeremymiller17 <jeremy.miller1@snhu.edu>
2022-07-15 22:10:14 -0400

    Removed static html and replaced with hbs templates

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,9 @@
-/* GET travel view */
-const travel = (req,res) => {
-    res.render('travel', {title: 'Travlr Getaways' });
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
+const travel = (req, res) => {
+  res.render("travel", { title: "Travlr Getaways", trips });
 };
 module.exports = {
-    travel
+  travel,
 };
