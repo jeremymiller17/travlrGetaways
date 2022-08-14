1de71e030369ec8c4ebbff2098471d454d28668b
jeremymiller17 <jeremy.miller1@snhu.edu>
2022-08-14 15:41:04 -0400

    Fixed issue, admin SPA now fully authenticates user, and allows for GET/PUSH request

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpModule } from "@angular/http";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
+
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-router.module";
@@ -31,7 +32,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule, 
-    HttpModule, 
+    HttpModule,
+    FormsModule, 
     ReactiveFormsModule, 
     AppRoutingModule],
   providers: [
